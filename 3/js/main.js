f4da3b4ab317d1ab86ba8f2b283d3b9ffb64d159
Keks <keks@htmlacademy.ru>
2022-10-10 21:16:43 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/main.js
@@ -0,0 +1,75 @@
+function getRandomNumber(min, max) {
+  if (min < 0 || max < 0 || max < min) {
+    return NaN;
+  }
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+
+function getRandomNumberWithPoint(min, max, amount) {
+  if (min < 0 || max < 0 || amount < 0 || max < min) {
+    return NaN;
+  }
+  return (Math.random() * (max - min) + min).toFixed(amount);
+}
+
+
+const offer = {
+  title : ['Выгодное предложение','разные плюшки', 'самый лучший отдых', 'Лучшее предложение этого месяца',],
+  adress : '{{location.lat}}, {{location.lng}}',
+  price : 0,
+  type : ['palace', 'flat', 'house', 'bungalow', 'hotel',],
+  rooms : 0,
+  guests :0,
+  checkin : ['12:00', '13:00', '14:00',],
+  checkout : ['12:00', '13:00', '14:00',],
+  features : ['wifi','dishwasher','parking', 'washer', 'elevator', 'conditioner'],
+  description : ['Кондиционер', 'тренажерный зал', 'спа процедуры','хороший вид',],
+  photos : ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'],
+};
+
+// lat, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000.
+
+// lng, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000.
+
+const x = getRandomNumberWithPoint(35.65000, 35.70000, 5);
+const y = getRandomNumberWithPoint(139.70000, 139.80000, 5);
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const SIMILAR_ADD_COUNT = 10;
+
+const createAdd = () => ({
+
+
+  author: {
+    avatar : `${getRandomNumber(0, 10) < 10 ? `img/avatars/user${0}${getRandomNumber(0, 10)}.png` : `img/avatars/user${getRandomNumber(0, 10)}.png}`
+    }`
+  },
+  location : {
+    lat : `${x}`,
+    lng : `${y}`,
+  },
+  offer : {
+    title :[getRandomArrayElement(offer.title)],
+    adress : `${x}, ${y}`,
+    price : getRandomNumber(1, 10),
+    type : [getRandomArrayElement(offer.type)],
+    rooms : getRandomNumber(1, 10),
+    guests :getRandomNumber(1, 10),
+    checkin : [getRandomArrayElement(offer.checkin)],
+    checkout : [getRandomArrayElement(offer.checkout)],
+    features : [getRandomArrayElement(offer.features)],
+    description : [getRandomArrayElement(offer.description)],
+    photos : [getRandomArrayElement(offer.photos)],
+  },
+
+});
+
+createAdd();
+
+// eslint-disable-next-line no-unused-vars
+const similarAdd = Array.from({length: SIMILAR_ADD_COUNT}, createAdd);
+
