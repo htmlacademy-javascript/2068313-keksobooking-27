7429e8d7df5289b227425400fa7884179314f5c5
Keks <keks@htmlacademy.ru>
2022-10-13 20:17:59 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,4 @@
+import {similarAdd} from './data.js';
+
+// eslint-disable-next-line no-console
+console.log(similarAdd);
---
+++ 4/js/util.js
@@ -0,0 +1,21 @@
+function getRandomNumber(min, max) {
+  if (min < 0 || max < 0 || max <= min) {
+    return NaN;
+  }
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+
+function getRandomNumberWithPoint(min, max, amount) {
+  if (min < 0 || max < 0 || amount < 0 || max < min) {
+    return NaN;
+  }
+  return (Math.random() * (max - min) + min).toFixed(amount);
+}
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+export {getRandomArrayElement};
+export {getRandomNumberWithPoint};
+export {getRandomNumber};
+
