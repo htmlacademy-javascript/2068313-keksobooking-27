d90aefb851f8c26c9a34a1fb40d6244c3b1a3663
Nikita Naumov <naumovn808@gmail.com>
2022-10-21 22:59:26 +0500

    add form.js

---
+++ js/form.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-expressions */
+const form = document.querySelector('.ad-form');
+const fieldsets = form.querySelectorAll('fieldset');
+
+const formDisable = () => {
+  form.classList.add('ad-form--disabled');
+  fieldsets.forEach((elem)=>{
+    elem.disabled - true;
+  });
+
+};
+
+formDisable();
+
+const formActive = () => {
+  form.classList.remove('ad-form--disabled');
+  fieldsets.forEach((elem)=>{
+    elem.disabled = false;
+  });
+
+};
+
+formActive();
