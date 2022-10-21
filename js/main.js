1470ad20b923e1c48d425a59e45bee2a49f27845
Nikita Naumov <naumovn808@gmail.com>
2022-10-21 22:15:59 +0500

    create card.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
+// eslint-disable-next-line no-unused-vars
 import {similarAdd} from './data.js';
+// eslint-disable-next-line no-unused-vars
+import {renderCard} from './card.js';
 
-// eslint-disable-next-line no-console
-console.log(similarAdd);
