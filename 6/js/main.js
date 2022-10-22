12b92b65c66d7bca348001ad89353d9c491a5a6f
Keks <keks@htmlacademy.ru>
2022-10-22 17:08:44 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import {similarAdd} from './data.js';
+// eslint-disable-next-line no-unused-vars
+import {renderCard} from './card.js';
+// eslint-disable-next-line no-unused-vars
+import {formDisable, formActive} from'./form.js';
+
