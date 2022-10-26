ef726cf0ee2195494086640299e9437c36b7f3e1
Nikita Naumov <naumovn808@gmail.com>
2022-10-26 17:56:55 +0500

    валидация формы

--- js/form.js
+++ js/form.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 /* eslint-disable no-unused-expressions */
 const form = document.querySelector('.ad-form');
 const fieldsets = form.querySelectorAll('fieldset');
@@ -21,4 +22,56 @@ const formActive = () => {
 };
 
 formActive();
-export {formDisable, formActive};
+
+const pristineTitle = new Pristine(form, {
+  classTo: 'ad-form__element--title',
+  errorTextParent: 'ad-form__element--title',
+  errorTextClass: 'ad-form__element--invalid',
+});
+
+// eslint-disable-next-line no-unused-vars
+const pristinePrice = new Pristine(form, {
+  classTo: 'ad-form__element--price',
+  errorTextParent: 'ad-form__element--price',
+  errorTextClass: 'ad-form__element--invalid',
+});
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element--room',
+  errorTextParent: 'ad-form__element--room',
+  errorTextClass: 'ad-form__element--invalid',
+} );
+
+const roomField = form.querySelector('[name="rooms"]');
+const capacityField = form.querySelector('[name="capacity"]');
+const roomsCapasityOption = {
+  '1' : ['для 1 гостя'],
+  '2' : ['для 2 гостей', 'для 1 гостя'],
+  '3' : ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
+  '100' : ['не для гостей'],
+};
+
+function validateroomsCapasityOption() {
+  return roomsCapasityOption[roomField.value].includes(capacityField.value);
+}
+
+// eslint-disable-next-line no-nested-ternary
+function getOptionErrorMessage () {
+  return `
+    ${roomField.value === '1' ? '1 комната для 1 гостя' :
+    roomField.value === '2' ? '2 комнтаты для 2 гостей или для 1 гостя' :
+      roomField.value === '3' ? '3 комнтаты для 3 гостей или для 2 гостей, или для 1 гостя' :
+        'не для гостей'}
+        `;
+}
+
+
+pristine.addValidator(roomField, validateroomsCapasityOption, getOptionErrorMessage );
+pristine.addValidator(capacityField, validateroomsCapasityOption, getOptionErrorMessage );
+
+form.addEventListener('submit', (evt) => {
+  const isValid = pristineTitle.validate();
+  if (!isValid) {
+    evt.preventDefault();
+  }
+});
--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ import {similarAdd} from './data.js';
 // eslint-disable-next-line no-unused-vars
 import {renderCard} from './card.js';
 // eslint-disable-next-line no-unused-vars
-import {formDisable, formActive} from'./form.js';
+import './form.js';
 
