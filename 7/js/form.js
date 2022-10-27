bff2d0b04c6b2851b9478c4b719327c7f2be7369
Keks <keks@htmlacademy.ru>
2022-10-27 19:43:55 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,90 @@
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-unused-expressions */
+const form = document.querySelector('.ad-form');
+const fieldsets = form.querySelectorAll('fieldset');
+
+const formDisable = () => {
+  form.classList.add('ad-form--disabled');
+  fieldsets.forEach((elem)=>{
+    elem.disabled - true;
+  });
+
+};
+
+formDisable();
+
+const formActive = () => {
+  form.classList.remove('ad-form--disabled');
+  fieldsets.forEach((elem)=>{
+    elem.disabled = false;
+  });
+
+};
+
+formActive();
+
+const pristineTitle = new Pristine(form, {
+  classTo: 'ad-form__element--title',
+  errorTextParent: 'ad-form__element--title',
+  errorTextClass: 'ad-form__element--invalid',
+});
+
+const pristinePrice = new Pristine(form, {
+  classTo: 'ad-form__element--price',
+  errorTextParent: 'ad-form__element--price',
+  errorTextClass: 'ad-form__element--invalid',
+});
+
+const FieldPrice = form.querySelector('#price');
+
+const pristineRoom = new Pristine(form, {
+  classTo: 'ad-form__element--room',
+  errorTextParent: 'ad-form__element--room',
+  errorTextClass: 'ad-form__element--invalid',
+} );
+
+const roomField = form.querySelector('[name="rooms"]');
+const capacityField = form.querySelector('[name="capacity"]');
+
+const roomsCapasityOption = {
+  '1' : ['для 1 гостя'],
+  '2' : ['для 2 гостей', 'для 1 гостя'],
+  '3' : ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
+  '100' : ['не для гостей'],
+};
+
+function validateroomsCapasityOption() {
+  return roomsCapasityOption[roomField.value].includes(capacityField.value);
+}
+
+// eslint-disable-next-line no-nested-ternary
+function getOptionErrorMessage () {
+  return `
+    ${roomField.value === '1' ? '1 комната для 1 гостя' :
+    roomField.value === '2' ? '2 комнтаты для 2 гостей или для 1 гостя' :
+      roomField.value === '3' ? '3 комнтаты для 3 гостей или для 2 гостей, или для 1 гостя' :
+        'не для гостей'}
+        `;
+}
+
+pristineRoom.addValidator(roomField, validateroomsCapasityOption, getOptionErrorMessage );
+pristineRoom.addValidator(capacityField, validateroomsCapasityOption, getOptionErrorMessage );
+
+
+FieldPrice.addEventListener('input', () => {
+  pristinePrice.validate();
+});
+
+roomField.addEventListener('change', () => {
+  pristineRoom.validate();
+});
+
+capacityField.addEventListener('change', () => {
+  pristineRoom.validate();
+});
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristineTitle.validate();
+  pristineRoom.validate();
+});
---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import {similarAdd} from './data.js';
+// eslint-disable-next-line no-unused-vars
+import {renderCard} from './card.js';
+// eslint-disable-next-line no-unused-vars
+import './form.js';
+
