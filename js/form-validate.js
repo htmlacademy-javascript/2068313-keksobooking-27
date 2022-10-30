b04c58e8c11bd140acc0199d16c262215fcecbbe
Nikita Naumov <naumovn808@gmail.com>
2022-10-30 15:06:46 +0500

    валидация формы

--- js/form-validate.js
+++ js/form-validate.js
@@ -1,14 +1,21 @@
-/* eslint-disable no-console */
 const form = document.querySelector('.ad-form');
 const capasityField = form.querySelector('#capacity');
 const roomField = form.querySelector('#room_number');
 
-const roomsCapasityOption = {
-  '1' : ['для 1 гостя'],
-  '2' : ['для 2 гостей', 'для 1 гостя'],
-  '3' : ['для 3 гостей', 'для 2 гостей', 'для 1 гостя'],
-  '100' : ['не для гостей'],
+const roomsOption = {
+  1 : ['1'],
+  2 : ['2', '1'],
+  3 : ['3', '2', '1'],
+  100 : ['0'],
 };
+
+const capacityOption = {
+  0 : ['100'],
+  1 : ['1'],
+  2 : ['1', '2'],
+  3 : ['3', '2', '1'],
+};
+
 
 const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
@@ -17,37 +24,28 @@ const pristine = new Pristine(form, {
 }, true);
 
 const validateCapasity = () => {
-  roomsCapasityOption[roomField.value].includes(capasityField.value);
+  roomsOption[roomField.value].includes(capasityField.value);
 };
 
-const getOptionErrorMessage = () => {
+const getCapacityErrorMessage = () =>
+  `Для указанного колличества гостей требуется ${capacityOption[capasityField.value].join(' или ')} комнат.`;
 
-  let result = '';
 
-  switch(roomField.value) {
-    case '1' : result = '1 комната для 1 гостя';
-      break;
-    case '2' : result = '2 комнтаты для 2 гостей или для 1 гостя';
-      break;
-    case '3' : result = '3 комнтаты для 3 гостей или для 2 гостей, или для 1 гостя';
-      break;
-    case '100' : result = 'не для гостей';
-      break;
-  }
-  return result;
-};
+const getRoomsErrorMessage = () =>
+  `указанное колличество комнат вмещает ${roomsOption[roomField.value].join(' или ')} гостей.`;
 
 pristine.addValidator(
   capasityField,
   validateCapasity,
-  '',
+  getCapacityErrorMessage,
 );
 
 pristine.addValidator(
   roomField,
   validateCapasity,
-  getOptionErrorMessage,
+  getRoomsErrorMessage ,
 );
+
 
 capasityField.addEventListener('change', () => {
   pristine.validate(capasityField);
@@ -61,11 +59,5 @@ roomField.addEventListener('change', () => {
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  const isValid = pristine.validate();
-
-  if(isValid) {
-    console.log('Можно отправлять');
-  }else {
-    console.log(pristine.getErrors());
-  }
+  pristine.validate();
 });
