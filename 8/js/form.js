43a65d7d97eacdb1c3d20a1aaec17042cc007f0f
Keks <keks@htmlacademy.ru>
2022-10-31 17:23:48 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/form.js
@@ -0,0 +1,25 @@
+import './form-validate.js';
+
+const form = document.querySelector('.ad-form');
+const fieldsets = form.querySelectorAll('fieldset');
+
+const formDisable = () => {
+  form.classList.add('ad-form--disabled');
+  fieldsets.forEach((elem)=>{
+    elem.disabled = true;
+  });
+
+};
+
+formDisable();
+
+const formActive = () => {
+  form.classList.remove('ad-form--disabled');
+  fieldsets.forEach((elem)=>{
+    elem.disabled = false;
+  });
+
+};
+
+formActive();
+
