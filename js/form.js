43d044494dcb3ffb6788c8f6e737a36d45365718
Nikita Naumov <naumovn808@gmail.com>
2022-11-13 22:48:17 +0500

    добавил кнопку очистки

--- js/form.js
+++ js/form.js
@@ -4,7 +4,6 @@ const form = document.querySelector('.ad-form');
 const fieldsets = form.querySelectorAll('fieldset');
 const adress = form.querySelector('#address');
 
-
 const formDisable = () => {
   form.classList.add('ad-form--disabled');
   fieldsets.forEach((elem)=>{
@@ -30,5 +29,6 @@ const NUMBER_POINT = 5;
 const setAdress = (coordinates) => {
   adress.value = `${coordinates.lat.toFixed(NUMBER_POINT)}, ${coordinates.lng.toFixed(NUMBER_POINT)}`;
 };
+
 
 export {formActive, formDisable, setAdress};
